2052b3855d6007bd6440995c8e51ee77a8aed0f7
Priyanshu1990 <priyanshuparate10@gmail.com>
2022-04-09 14:36:30 +0530

    added login and register scketon

---
+++ src/compoents/header/LoginRegisterHeader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const LoginRegisterHeader = () => {
+  return (
+    <div className="container b font-mono text-center h-30">
+      Petfed Login/Register
+    </div>
+  );
+};
